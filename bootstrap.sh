dbd77bd737b8fd6a5fd38d6bae2368aa0098974b
Eduardo Grajeda <tatofoo@gmail.com>
2015-06-01 20:00:48 -0300

    Actualizado Vim's Vundle.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,8 @@ DOTFILES=$(cd $(dirname $0); pwd -P)
 
 ln -s $DOTFILES/tmux.conf ~/.tmux.conf
 ln -s $DOTFILES/gitconfig ~/.gitconfig
+
+git submodule update --init --recursive
+ln -s $DOTFILES/vimrc ~/.vimrc
+ln -sT $DOTFILES/vim ~/.vim
+vim +PluginInstall +qall
